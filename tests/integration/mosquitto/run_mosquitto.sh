ebc822484b6e70d4f9080d9d46828275965cd7fb
Pauli Anttila <pauli.anttila@gmail.com>
2025-06-01 08:57:16 +0300

    Fixed paths

--- tests/integration/mosquitto/run_mosquitto.sh
+++ tests/integration/mosquitto/run_mosquitto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Set password for tavern and myapp"
-mosquitto_passwd -b src/tests/integration/mosquitto/mosquitto.passwd tavern tavern123
-mosquitto_passwd -b src/tests/integration/mosquitto/mosquitto.passwd myapp myapp123
+mosquitto_passwd -b tests/integration/mosquitto/mosquitto.passwd tavern tavern123
+mosquitto_passwd -b tests/integration/mosquitto/mosquitto.passwd myapp myapp123
 
 echo "Run mosquitto"
-mosquitto -c src/tests/integration/mosquitto/mosquitto.conf &
+mosquitto -c tests/integration/mosquitto/mosquitto.conf &
